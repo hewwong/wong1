9e42ee14fafb532feed8f6c036fd0145952041a6
hew <olaio@qq.com>
2014-02-20 15:33:51 +0800

    Initial commit

---
+++ do1/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Do1::Application.config.secret_key_base = '4f76712cd2d3df0954d280583a5e7f2691a8d7a2718d2386cdc54efeb314a4fd6cbccfb2dd57338b603accb1f1061f24610c505e117431e0e5bef3c9ad1ea08e'
---
+++ do1/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Do1::Application.config.session_store :cookie_store, key: '_do1_session'
